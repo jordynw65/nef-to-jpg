6fd42eca2f110b60eb4f16037a5a35048ee26971
Jordyn Wright <98404383+jewright@users.noreply.github.com>
2022-07-29 08:37:13 -0400

    Update main.py

--- photoconverter/main.py
+++ photoconverter/main.py
@@ -6,8 +6,8 @@ import time
 print("Starting Now")
 start = time.time()
 # Sometimes the file ending can be .nef or .NEF, therefore I included both possibilities to save extra work.
-pathnef = "/Users/jordyn/Desktop/Jordyn/photoconverter/Single Image/*.nef"
-pathNEF = "/Users/jordyn/Desktop/Jordyn/photoconverter/Single Image/*.NEF"
+pathnef = "/path/to/image/folder/*.nef" # KEEP /.nef AFTER PATH
+pathNEF = "/path/to/image/folder/*.NEF" # KEEP /.NEF AFTER PATH
 for path in glob.glob(pathnef):
     with rawpy.imread(path) as raw:
         rgb = raw.postprocess()
@@ -19,5 +19,4 @@ for path in glob.glob(pathNEF):
         imageio.imwrite(path + '.jpg', rgb)
 
 end = time.time()
-print("Elapsed Time:")
-print(round(end - start, 2), "seconds")
+print("Elapsed Time:",round(end - start, 2), "seconds")
