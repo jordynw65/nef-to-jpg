4592ba6798dbb645d76a2a1573ead8a9e0922f83
Jordyn <98404383+jewright@users.noreply.github.com>
2022-11-14 23:30:59 -0500

    Removal of nef or NEF within new file name.

--- photoconverter/main.py
+++ photoconverter/main.py
@@ -6,25 +6,29 @@ import time
 
 def main():
     # Timer is optional
-    # print("Starting Now")
-    # start = time.time()
-    
+    print("Starting Now")
+    start = time.time()
+
     # Sometimes the file ending can be .nef or .NEF, therefore I included both possibilities to save extra work.
-    pathnef = "/folderpath/*.nef"
-    pathNEF = "/folderpath/*.NEF"
+    pathnef = "/path/to/*.nef"
+    pathNEF = "/path/to/*.NEF"
     for path in glob.glob(pathnef):
         with rawpy.imread(path) as raw:
             rgb = raw.postprocess()
-            imageio.imwrite(path + '.jpg', rgb)
+
+            imageio.imwrite(path.replace('.nef', '') + '.jpg', rgb)
 
     for path in glob.glob(pathNEF):
         with rawpy.imread(path) as raw:
             rgb = raw.postprocess()
-            imageio.imwrite(path + '.jpg', rgb)
-    
+            imageio.imwrite(path.replace('.NEF', '') + '.jpg', rgb)
+
     # Timer is optional
-    # end = time.time()
-    # print("Elapsed Time:", round(end - start, 2), "seconds")
+    end = time.time()
+    minutes = (end - start) / 60
+    seconds = (end - start) % 60
+
+    print("Elapsed Time:", round(minutes), "minutes and ", round(seconds), "seconds")
 
 
 if __name__ == '__main__':
